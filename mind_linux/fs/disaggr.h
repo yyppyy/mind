81dcf250e27f37416167c6aaef2ca77e18cc0559
rRossJ24 <rossjohnsonpro@outlook.com>
2021-11-29 13:51:17 -0800

    Fixing compilation errors

--- mind_linux/fs/disaggr.h
+++ mind_linux/fs/disaggr.h
@@ -1,3 +1,5 @@
+
+#include <linux/slab.h>
 #ifndef __DISAGGR_H__
 #define __DISAGGR_H__
 // #include <disagg/network_disagg.h>
@@ -11,39 +13,39 @@ extern int send_msg_to_memory(u32 msg_type, void *payload, u32 len_payload,
                         void *retbuf, u32 max_len_retbuf);
 
 int disaggr_open_file(const char __user *filename, int flags, umode_t mode ){
-    open_file_req_t request;
-    open_file_res_t response;
-    strcpy(request.path, filename);
-    request.flags = flags;
-    request.mode = mode;
-    send_msg_to_memory(MT_OPEN, &request, sizeof(request),&response, sizeof(&response));
+    open_file_req_t* request = kmalloc(sizeof(open_file_req_t));
+    open_file_res_t* response = kmalloc(sizeof(open_file_res_t));
+    strcpy(request->path, filename);
+    request->flags = flags;
+    request->mode = mode;
+    send_msg_to_memory(MT_OPEN, request, sizeof(*request), response, sizeof(*response));
     return response.file_descriptor;
 }
 
 int disaggr_read_file(unsigned int fd, char* usr_buf, size_t num_bytes){
-    read_file_req_t request;
-    read_file_res_t response;
-    request.fd = fd;
-    request.num_bytes = num_bytes;
-    send_msg_to_memory(MT_READ, &request, sizeof(request),&response, sizeof(&response));
-    strncpy(usr_buf,response.read_buf, response.bytes_read);
-    return response.bytes_read;
+    read_file_req_t* request = kmalloc(sizeof(read_file_req_t));
+    read_file_res_t* response = kmalloc(sizeof(read_file_res_t));
+    request->fd = fd;
+    request->num_bytes = num_bytes;
+    send_msg_to_memory(MT_READ, request, sizeof(*request),response, sizeof(*response));
+    strncpy(usr_buf, response->read_buf, response->bytes_read);
+    return response->bytes_read;
 }
 
 int disaggr_write_file(unsigned int fd, char* usr_buf, size_t num_bytes){
-    write_file_req_t request;
-    write_file_res_t response;
-    request.num_chars = num_bytes;
-    send_msg_to_memory(MT_WRITE, &request, sizeof(request),&response, sizeof(&response));
-    return response.bytes_written;
+    write_file_req_t* request = kmalloc(sizeof(write_file_req_t));
+    write_file_res_t* response = kmalloc(sizeof(write_file_res_t));
+    request->num_chars = num_bytes;
+    send_msg_to_memory(MT_WRITE, request, sizeof(*request),response, sizeof(*response));
+    return response->bytes_written;
 }
 
 int disaggr_close_file(unsigned int fd){
-    close_file_req_t request;
-    close_file_res_t response;
+    close_file_req_t* request = kmalloc(sizeof(close_file_req_t));
+    close_file_res_t* response = kmalloc(sizeof(close_file_res_t));
     request.fd = fd;
-    send_msg_to_memory(MT_CLOSE, &request, sizeof(request),&response, sizeof(&response));
-    return response.closed;
+    send_msg_to_memory(MT_CLOSE, request, sizeof(*request), response, sizeof(*response));
+    return response->closed;
 }
 
 #endif
