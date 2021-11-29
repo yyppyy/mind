2cf3005d9aba5ec50886c7b9122cccc5d13e5e51
rRossJ24 <rossjohnsonpro@outlook.com>
2021-11-29 13:54:09 -0800

    Fixing compilation errors

--- mind_linux/fs/disaggr.h
+++ mind_linux/fs/disaggr.h
@@ -13,8 +13,8 @@ extern int send_msg_to_memory(u32 msg_type, void *payload, u32 len_payload,
                         void *retbuf, u32 max_len_retbuf);
 
 int disaggr_open_file(const char __user *filename, int flags, umode_t mode ){
-    open_file_req_t* request = kmalloc(sizeof(open_file_req_t));
-    open_file_res_t* response = kmalloc(sizeof(open_file_res_t));
+    open_file_req_t* request = kmalloc(sizeof(open_file_req_t), GFP_USER);
+    open_file_res_t* response = kmalloc(sizeof(open_file_res_t), GFP_USER);
     strcpy(request->path, filename);
     request->flags = flags;
     request->mode = mode;
@@ -23,8 +23,8 @@ int disaggr_open_file(const char __user *filename, int flags, umode_t mode ){
 }
 
 int disaggr_read_file(unsigned int fd, char* usr_buf, size_t num_bytes){
-    read_file_req_t* request = kmalloc(sizeof(read_file_req_t));
-    read_file_res_t* response = kmalloc(sizeof(read_file_res_t));
+    read_file_req_t* request = kmalloc(sizeof(read_file_req_t), GFP_USER);
+    read_file_res_t* response = kmalloc(sizeof(read_file_res_t), GFP_USER);
     request->fd = fd;
     request->num_bytes = num_bytes;
     send_msg_to_memory(MT_READ, request, sizeof(*request),response, sizeof(*response));
@@ -33,17 +33,17 @@ int disaggr_read_file(unsigned int fd, char* usr_buf, size_t num_bytes){
 }
 
 int disaggr_write_file(unsigned int fd, char* usr_buf, size_t num_bytes){
-    write_file_req_t* request = kmalloc(sizeof(write_file_req_t));
-    write_file_res_t* response = kmalloc(sizeof(write_file_res_t));
+    write_file_req_t* request = kmalloc(sizeof(write_file_req_t), GFP_USER);
+    write_file_res_t* response = kmalloc(sizeof(write_file_res_t), GFP_USER);
     request->num_chars = num_bytes;
     send_msg_to_memory(MT_WRITE, request, sizeof(*request),response, sizeof(*response));
     return response->bytes_written;
 }
 
 int disaggr_close_file(unsigned int fd){
-    close_file_req_t* request = kmalloc(sizeof(close_file_req_t));
-    close_file_res_t* response = kmalloc(sizeof(close_file_res_t));
-    request.fd = fd;
+    close_file_req_t* request = kmalloc(sizeof(close_file_req_t), GFP_USER);
+    close_file_res_t* response = kmalloc(sizeof(close_file_res_t), GFP_USER);
+    request->fd = fd;
     send_msg_to_memory(MT_CLOSE, request, sizeof(*request), response, sizeof(*response));
     return response->closed;
 }
