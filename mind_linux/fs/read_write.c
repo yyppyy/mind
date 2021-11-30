c60aee9b5506435ae92188a492099203a914e72c
rRossJ24 <rossjohnsonpro@outlook.com>
2021-11-30 13:17:11 -0800

    Debugging

--- mind_linux/fs/read_write.c
+++ mind_linux/fs/read_write.c
@@ -571,7 +571,7 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	if (current_uid().val == 1002)
 		printk("hello I'm sys_read, called by %s\n", current->comm);
 	if(fdset_contains(fd)){
-		KERN_PANIC("NOT SUPPOSED TO BE HERE");
+		panic("NOT SUPPOSED TO BE HERE\n");
 		return disaggr_read_file(fd, buf, count);
 	}
 	struct fd f = fdget_pos(fd);
