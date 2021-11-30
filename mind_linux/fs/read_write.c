75560c6c92d64c09986d14a5981cb7e01fdb1838
rRossJ24 <rossjohnsonpro@outlook.com>
2021-11-30 12:32:40 -0800

    Debugging

--- mind_linux/fs/read_write.c
+++ mind_linux/fs/read_write.c
@@ -570,9 +570,9 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	if (current_uid().val == 1002)
 		printk("hello I'm sys_read, called by %s\n", current->comm);
-	// if(fdset_contains(fd)){
-	// 	return disaggr_read_file(fd, buf, count);
-	// }
+	if(fdset_contains(fd)){
+		return disaggr_read_file(fd, buf, count);
+	}
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
 
