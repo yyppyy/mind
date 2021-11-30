23905bb9e6fc535fd5f6f828df6b0dc81384748a
rRossJ24 <rossjohnsonpro@outlook.com>
2021-11-29 16:29:51 -0800

    Fixing compilation errors

--- mind_linux/fs/disaggr_fs.c
+++ mind_linux/fs/disaggr_fs.c
@@ -12,7 +12,10 @@ int disaggr_open_file(const char __user *filename, int flags, umode_t mode ){
     request->flags = flags;
     request->mode = mode;
     send_msg_to_memory(MT_OPEN, request, sizeof(*request), response, sizeof(*response));
-    return response->file_descriptor;
+    int fd = response->file_descriptor;
+    kfree(request);
+    kfree(response);
+    return fd;
 }
 
 int disaggr_read_file(unsigned int fd, char* usr_buf, size_t num_bytes){
@@ -22,7 +25,10 @@ int disaggr_read_file(unsigned int fd, char* usr_buf, size_t num_bytes){
     request->num_bytes = num_bytes;
     send_msg_to_memory(MT_READ, request, sizeof(*request),response, sizeof(*response));
     strncpy(usr_buf, response->read_buf, response->bytes_read);
-    return response->bytes_read;
+    int bytes_read = response->bytes_read;
+    kfree(request);
+    kfree(response);
+    return bytes_read;
 }
 
 int disaggr_write_file(unsigned int fd, char* usr_buf, size_t num_bytes){
@@ -30,7 +36,10 @@ int disaggr_write_file(unsigned int fd, char* usr_buf, size_t num_bytes){
     write_file_res_t* response = kmalloc(sizeof(write_file_res_t), GFP_USER);
     request->num_chars = num_bytes;
     send_msg_to_memory(MT_WRITE, request, sizeof(*request),response, sizeof(*response));
-    return response->bytes_written;
+    int bytes_written = response->bytes_written;
+    kfree(request);
+    kfree(response);
+    return bytes_written;
 }
 
 int disaggr_close_file(unsigned int fd){
@@ -38,5 +47,8 @@ int disaggr_close_file(unsigned int fd){
     close_file_res_t* response = kmalloc(sizeof(close_file_res_t), GFP_USER);
     request->fd = fd;
     send_msg_to_memory(MT_CLOSE, request, sizeof(*request), response, sizeof(*response));
-    return response->closed;
+    int closed = response->closed;
+    kfree(request);
+    kfree(response);
+    return closed;
 }
