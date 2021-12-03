4ff945244085a3cc95f7b3f68ce036668b0d29bd
rRossJ24 <rossjohnsonpro@outlook.com>
2021-12-02 22:03:51 -0800

    Debugging

--- mind_linux/fs/disaggr_fs.c
+++ mind_linux/fs/disaggr_fs.c
@@ -51,8 +51,8 @@ int disaggr_read_file(unsigned int fd, char __user* usr_buf, size_t num_bytes){
 int disaggr_write_file(unsigned int fd, char* usr_buf, size_t num_bytes){
     printk("hwrite\n");
     printk_safe_flush();
-    write_file_req_t* request = kmalloc(sizeof(write_file_req_t), GFP_USER);
-    write_file_res_t* response = kmalloc(sizeof(write_file_res_t), GFP_USER);
+    write_file_req_t* request = kmalloc(sizeof(write_file_req_t), GFP_KERNEL);
+    write_file_res_t* response = kmalloc(sizeof(write_file_res_t), GFP_KERNEL);
     request->num_chars = num_bytes;
     send_msg_to_memory(MT_WRITE, request, sizeof(*request),response, sizeof(*response));
     int bytes_written = response->bytes_written;
@@ -64,8 +64,8 @@ int disaggr_write_file(unsigned int fd, char* usr_buf, size_t num_bytes){
 int disaggr_close_file(unsigned int fd){
     printk("hclose\n");
     printk_safe_flush();
-    close_file_req_t* request = kmalloc(sizeof(close_file_req_t), GFP_USER);
-    close_file_res_t* response = kmalloc(sizeof(close_file_res_t), GFP_USER);
+    close_file_req_t* request = kmalloc(sizeof(close_file_req_t), GFP_KERNEL);
+    close_file_res_t* response = kmalloc(sizeof(close_file_res_t), GFP_KERNEL);
     request->fd = fd;
     send_msg_to_memory(MT_CLOSE, request, sizeof(*request), response, sizeof(*response));
     int closed = response->closed;
