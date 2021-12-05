5879c38235319cb12df7a5e20d5fcfa31dc754b0
rRossJ24 <rossjohnsonpro@outlook.com>
2021-12-05 09:43:06 -0800

    Debugging

--- mind_linux/fs/disaggr_fs.c
+++ mind_linux/fs/disaggr_fs.c
@@ -57,9 +57,10 @@ int disaggr_read_file(unsigned int fd, char __user* usr_buf, size_t num_bytes){
     memset(request, 0, sizeof(*request));
     request->fd = fd;
     request->num_bytes = num_bytes;
-    send_msg_to_memory(MT_READ, request, sizeof(*request),response, sizeof(*response));
-    printk("READ: %s", response->read_buf);
-    printk("BYTES READ: %d", response->bytes_read);
+    int mind_res = send_msg_to_memory(MT_READ, request, sizeof(*request),response, sizeof(*response));
+    printk("MIND READ RES: %d\n", mind_res);
+    printk("READ: %s\n", response->read_buf);
+    printk("BYTES READ: %d\n", response->bytes_read);
     copy_to_user(usr_buf, response->read_buf, response->bytes_read);
     int bytes_read = response->bytes_read;
     kfree(request);
